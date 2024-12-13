7e987d0eddc9bdce478d59a66220d6562b7b1d1a
Setiawan Indra <zetiawanindra@gmail.com>
2024-12-14 05:41:39 +0700

    update logic capitalize

--- capitalize(backup)/script.js
+++ capitalize(backup)/script.js
@@ -7,19 +7,13 @@ function capitalizeText() {
     let lines = originalText.split("\n");
 
     for (let i = 0; i < lines.length; i++) {
-      // Hapus penomoran di awal dan akhir baris
-      lines[i] = removeLeadingAndTrailingNumbering(lines[i]);
-
-      // Modifikasi setelah karakter spesial
       lines[i] = modifyAfterSpecialCharacters(lines[i]);
 
-      // Capitalize jika tidak diawali tanda tertentu
       if (
         lines[i].trim().startsWith("“") ||
         lines[i].trim().startsWith("(") ||
         lines[i].trim().startsWith("―")
       ) {
-        continue; // Abaikan untuk baris ini
       } else {
         lines[i] = capitalizeFirstLetter(lines[i]);
       }
@@ -39,12 +33,6 @@ function modifyAfterSpecialCharacters(str) {
   return str.replace(/([“(])(\w)/g, (match, p1, p2) => {
     return p1 + p2.toUpperCase();
   });
-}
-
-function removeLeadingAndTrailingNumbering(str) {
-  return str
-    .replace(/^\d+\.\s*/, "") // Menghapus angka diikuti titik dan spasi di awal baris
-    .replace(/\d+\.$/, ""); // Menghapus angka diikuti titik di akhir baris
 }
 
 function copyText() {
